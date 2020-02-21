2dec8fb0163b3202466ce8c970da906d02c3cba5
prakashelango <prakashcse100@gmail.com>
2020-02-21 10:21:06 +0530

    Pre-init

---
+++ src/main/java/com/base/movingwalls/controller/LoginController.java
@@ -0,0 +1,7 @@
+package com.base.movingwalls.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+}
---
+++ src/main/java/com/base/movingwalls/model/login/Login.java
@@ -0,0 +1,4 @@
+package com.base.movingwalls.model.login;
+
+public class Login {
+}
