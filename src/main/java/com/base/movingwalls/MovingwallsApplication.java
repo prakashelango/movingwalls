888ece014104535eb6bb17d1c695be117c80730f
prakashelango <prakashcse100@gmail.com>
2020-02-21 09:59:10 +0530

    Directory Creation

---
+++ src/main/java/com/base/movingwalls/MovingwallsApplication.java
@@ -0,0 +1,13 @@
+package com.base.movingwalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingwallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovingwallsApplication.class, args);
+	}
+
+}
